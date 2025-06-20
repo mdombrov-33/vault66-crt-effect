87222c9b8e9542f38f6998297c6026df99d62688
Maksym Dombrov <maksymdombrov@gmail.com>
2025-06-20 13:01:25 +0300

    fix: fix deploy bugs

--- src/components/CRTEffect.tsx
+++ src/components/CRTEffect.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import "../styles/crt.css";
 
 interface CRTEffectProps {
   enabled?: boolean;
--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,2 @@
-import "./styles/crt.css";
 import CRTEffect from "./components/CRTEffect";
 export default CRTEffect;
